0611d758503c241695566995093c760422879ac4
nasrimohtadi <mohtadi-nasri@forcus-corporation.com>
2021-10-16 00:07:17 +0200

    MultilineTemplate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  template: '<div><h1>{{pageTitle}}</h1><div>My First Component</div></div>'
+  template: `
+  <div>
+    <h1>{{pageTitle}}</h1>
+    <div>My First Component</div>
+  </div>`
 })
 export class AppComponent {
 
